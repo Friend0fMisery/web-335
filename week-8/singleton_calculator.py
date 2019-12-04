ee633ae7a916dd40c488743c3572f10eba409cf9
Justin Singleton <jsingleton@my365.bellevue.edu>
2019-12-03 19:02:05 -0600

    Add assignment8.2, 8.3

---
+++ week-8/singleton_calculator.py
@@ -0,0 +1,12 @@
+def add(v1, v2):
+    return v1 + v2
+
+def subtract(v1, v2):
+    return v1 - v2
+
+def divide(v1, v2):
+    return v1 / v2
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
